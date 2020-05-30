5759eef2b3cc1b8827e05fc9f2079ba78a751c19
qwertysampletext <47921724+qwertysampletext@users.noreply.github.com>
2020-05-30 13:28:39 -0500

    Add files via upload

--- !brstm_change_rate.py
+++ !brstm_change_rate.py
@@ -5,10 +5,20 @@ brstm_list = glob('*_n.brstm')
 
 for file in brstm_list:
  brstm_n = open(file,'rb')
+ if brstm_n.read(6) == b'RSTM\xfe\xff':
+  endianness = 'big'
+ elif brstm_n.read(6) == b'RSTM\xff\xfe':
+  endianness = 'little'
+ else:
+  print('Invalid file.  ')
+  quit()
+ brstm_n.seek(0)
  brstm_f = open(f'{file.rstrip("_n.brstm")}_f.brstm','wb')
  brstm_f.write(brstm_n.read(0x64))
- rate_old = int.from_bytes(brstm_n.read(2),byteorder='big')
+ rate_old = int.from_bytes(brstm_n.read(2),byteorder=endianness)
  rate_new = int(rate_old * RATE_MULTIPLIER)
- brstm_f.write(rate_new.to_bytes(2,byteorder='big'))
+ brstm_f.write(rate_new.to_bytes(2,byteorder=endianness))
  brstm_f.write(brstm_n.read())
- print(f'{file}: old rate {rate_old}Hz, new rate {rate_new}Hz. ')
+ brstm_n.close()
+ brstm_f.close()
+ print(f'{file}: endianness {endianness}, original rate {rate_old}Hz, new rate {rate_new}Hz. ')
