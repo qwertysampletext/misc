2a93398d28f599df13450ecf2c3b9b0bc5c07127
qwertysampletext <47921724+qwertysampletext@users.noreply.github.com>
2020-06-05 10:49:37 -0500

    Add files via upload

---
+++ !test01.py
@@ -0,0 +1,10 @@
+#https://pastebin.com/raw/KNLvDr9i
+list_file = open('list.txt','r')
+
+for i,line in enumerate(list_file,1):
+ data = line[line.rfind('/') + 1:].rstrip("\n").split(" - ")
+ points = data[1].split(' ')
+ if int(points[1],10) != 0:
+  txtp_name = f'{data[0]}#I{points[0]} {points[1]}.txtp'
+  print(f'{str(i).zfill(4)}: {txtp_name}')
+  open(txtp_name,'wb').close()
